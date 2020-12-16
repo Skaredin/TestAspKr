80aa8eeb6b3f71c230b35eb9edcbe751115e7ff5
Sasha <sksv94@mail.ru>
2020-12-16 18:51:11 +0500

    ЗАлил

--- TestT/Controllers/MenuController.cs
+++ TestT/Controllers/MenuController.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using TestT.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using System.Configuration;
 
 namespace TestT.Controllers
 {
     public class MenuController : Controller
     {
+        private TestAspEntities1 db = new TestAspEntities1();
         // GET: Menu
         public ActionResult Index()
         {
@@ -86,19 +95,38 @@ namespace TestT.Controllers
             }
         }
 
-        // GET: Menu/Delete/5
-        public ActionResult _MainMenu(int id)
+        public ActionResult _MainMenu()
         {
-            return View();
+
+            menuMain M = new menuMain();
+            Dictionary<int, string> projects = new Dictionary<int, string>();
+            Dictionary<long, string> accounts = new Dictionary<long, string>();
+
+            var uid = User.Identity.GetUserId();
+
+            var acc = db.Accounts.Where(a => a.Accounts_Users.Select(u => u.AspNetUsers.Id).Contains(uid));
+
+            List<ItemMenuProfile> itemMenuProfile = db.Projects
+                .Where(p => p.Accounts.Accounts_Users
+                .Select(u => u.AspNetUsers.Id)
+                .Contains(uid))
+                .Select(i => new ItemMenuProfile { id = i.id, name = i.name, guid = i.guid }).ToList();
+
+            foreach (var item in db.Projects.Where(p => acc.Select(a => a.Id).Contains(p.accountId)))
+            {
+                projects.Add(item.id, item.name);
+            }
+
+            foreach (var item in acc)
+            {
+                accounts.Add(item.Id, item.ShortLegalName);
+            }
+
+            M.menuProject = projects;
+            M.menuAccaunts = accounts;
+            M.menuProfile = itemMenuProfile;
+            return PartialView("_MainMenu", M);
         }
 
-        // POST: Menu/Delete/5
-        [HttpPost]
-        public ActionResult _MainMenu(int id, FormCollection collection)
-        {
-            return View();
-
-        }
-      
     }
 }
